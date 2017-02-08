f70aedd53c7aa84b0d2e6aec2e36e400413f7629
flupe <lucas@escot.me>
2017-02-08 12:13:45 +0100

    expr to string conversion

--- src/expr.ml
+++ src/expr.ml
@@ -1,8 +1,18 @@
 type expr =
-  | Const of bool
+  | Var of int
   | And of expr * expr
   | Or of expr * expr
   | Xor of expr * expr
   | Not of expr
   | Implies of expr * expr
   | Equiv of expr * expr
+
+let rec string_of_expr = function
+  | Var x -> string_of_int x
+  | And (l, r) -> "(" ^ string_of_expr l ^ ") /\\ (" ^ string_of_expr r ^ ")"
+  | Or (l, r) -> "(" ^ string_of_expr l ^ ") \\/ (" ^ string_of_expr r ^ ")"
+  | Xor (l, r) -> "(" ^ string_of_expr l ^ ") X (" ^ string_of_expr r ^ ")"
+  | Not e -> "~(" ^ string_of_expr e ^ ")" 
+  | Implies (l, r) -> "(" ^ string_of_expr l ^ ") => (" ^ string_of_expr r ^ ")"
+  | Equiv (l, r) -> "(" ^ string_of_expr l ^ ") <=> (" ^ string_of_expr r ^ ")"
+
